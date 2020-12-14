2c99d729776e7f054727cf64d9a1460691e0698a
kpeenee <43548093+kpeenee@users.noreply.github.com>
2020-12-14 15:32:13 +0000

    Setting up database

--- routes/login.js
+++ routes/login.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const User = require('./../models/user');
 const router = express.Router();
 
 router.get('/' , (req, res) =>{
@@ -9,9 +10,21 @@ router.get('/signup'  , (req,res) =>{
     res.render('signUp');
 });
 
-router.post('/signup' , (req,res) =>{
-    console.log("Yep");
-    res.render('login');
+router.get('/:id' ,(req,res) => {
+    res.send(req.params.id);
+})
+
+router.post('/signup' , async (req,res) =>{
+    let user = new User({
+        username: req.body.username,
+        password: req.body.password
+    })
+    try{
+    user = await user.save();
+    res.redirect(`/login/${user.id}`);
+    }catch (e) {
+        res.render('signup');
+    }
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const loginRoute = require('./routes/login');
+const mongoose = require('mongoose');
+const { urlencoded } = require('express');
 const app = express();
+
+mongoose.connect('mongodb://localhost/users',{ useNewUrlParser: true, useUnifiedTopology: true })
 
 app.set('view engine', 'ejs');
 
+app.use(urlencoded({extended: false}));
 app.use('/login', loginRoute);
+
 
 app.get('/', (req,res) =>{
     res.render('index');
